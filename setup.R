13c0b3b1a21f943e281ccb2bd5edc71f3a0a111c
Alexander Toenges <atpoint90@gmail.com>
2023-05-14 12:21:18 +0000

    update to 3.17

--- setup.R
+++ setup.R
@@ -6,7 +6,7 @@ options(repos=c(CRAN="https://cloud.r-project.org"))
 
 install.packages("pak")
 
-pkgs <- c("renv", "RhpcBLASctl", "rmdformats", "bookdown", "markdown", "rmarkdown", "remotes")
+pkgs <- c("renv", "RhpcBLASctl", "rmdformats", "bookdown", "markdown", "rmarkdown", "remotes", "devtools")
 pak::pkg_install(pkgs)
 pak::cache_clean()
 
